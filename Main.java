f25e39a1735b676acbf2691e9d655bc1db96644f
aiti25 <baidyaaitijhya@gmail.com>
2026-02-23 20:50:37 +0530

    fixed the error of bank account code

--- Main.java
+++ Main.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 class BankAccount {
 
-    private String name;
     private double balance;
 
     // Constructor
     BankAccount(String name, double balance) {
-        this.name = name;
         this.balance = balance;
+    }
+
+    public BankAccount() {
     }
 
     // Deposit method
@@ -41,54 +42,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter Account Holder Name: ");
-        String name = sc.nextLine();
-
-        System.out.print("Enter Initial Balance: ");
-        double balance = sc.nextDouble();
-
-        BankAccount account = new BankAccount(name, balance);
-
-        int choice;
-
-        do {
-            System.out.println("\n1. Deposit");
-            System.out.println("2. Withdraw");
-            System.out.println("3. Check Balance");
-            System.out.println("4. Exit");
-            System.out.print("Enter choice: ");
-
-            choice = sc.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter deposit amount: ");
-                    double dep = sc.nextDouble();
-                    account.deposit(dep);
-                    break;
-
-                case 2:
-                    System.out.print("Enter withdraw amount: ");
-                    double wit = sc.nextDouble();
-                    account.withdraw(wit);
-                    break;
-
-                case 3:
-                    account.displayBalance();
-                    break;
-
-                case 4:
-                    System.out.println("Thank you!");
-                    break;
-
-                default:
-                    System.out.println("Invalid choice!");
-            }
-
-        } while (choice != 4);
-
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter Account Holder Name: ");
+            String name = sc.nextLine();
+            
+            System.out.print("Enter Initial Balance: ");
+            double balance = sc.nextDouble();
+            
+            BankAccount account = new BankAccount(name, balance);
+            
+            int choice;
+            
+            do {
+                System.out.println("\n1. Deposit");
+                System.out.println("2. Withdraw");
+                System.out.println("3. Check Balance");
+                System.out.println("4. Exit");
+                System.out.print("Enter choice: ");
+                
+                choice = sc.nextInt();
+                
+                switch (choice) {
+                    case 1 -> {
+                        System.out.print("Enter deposit amount: ");
+                        double dep = sc.nextDouble();
+                        account.deposit(dep);
+                    }
+                    
+                    case 2 -> {
+                        System.out.print("Enter withdraw amount: ");
+                        double wit = sc.nextDouble();
+                        account.withdraw(wit);
+                    }
+                    
+                    case 3 -> account.displayBalance();
+                    
+                    case 4 -> System.out.println("Thank you!");
+                    
+                    default -> System.out.println("Invalid choice!");
+                }
+                
+            } while (choice != 4);
+        }
     }
 }
